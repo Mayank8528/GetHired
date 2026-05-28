8d373c4b137de41067dc85099c808b1120e948e3
Mayank Srivastava <156085416+Mayank8528@users.noreply.github.com>
2026-05-28 15:52:40 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gethired-backend-0rdn.onrender.com",
     withCredentials: true,
 })
 
